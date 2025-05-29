f01bccc22cede9b3280318576bee6f11c118bb4b
juninbr2000 <edson.gentil.junior@gmail.com>
2025-05-29 13:29:47 -0300

    images fixeds

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
-  }
-};
-
-module.exports = {
-  images: {
     domains: ['image.tmdb.org'],
   }
-}
+};
 
 export default nextConfig;
